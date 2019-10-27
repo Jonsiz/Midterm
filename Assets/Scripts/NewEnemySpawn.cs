7887dfb2e6c3191c979161197e3f7c91cf7aaec4
Jonsiz <connor.perfect@loop.colum.edu>
2019-10-27 18:43:06 -0500

    Updated to Build #3
    Game has a UI, Sound, Fixed the playtest problem with people immidiately going near the track and throwing the car off course. Learned Unity terrain tools suck.

---
+++ Assets/Scripts/NewEnemySpawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewEnemySpawn : MonoBehaviour
+{
+    public GameObject[] carPrefabs;
+    public int carIndex;
+
+    private void OnTriggerEnter()
+    {
+        Instantiate(carPrefabs[carIndex], new Vector3(197, 4, 43), carPrefabs[carIndex].transform.rotation);
+    }
+} 
